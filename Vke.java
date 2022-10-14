0dc56a534364e7e6afe18bafe535de346480408f
atsydev <108891562+atsydev@users.noreply.github.com>
2022-10-14 21:46:47 +0300

    vke programı

---
+++ Vke.java
@@ -0,0 +1,18 @@
+package Uygulamalar;
+
+import java.util.Scanner;
+
+public class Vke {
+    public static void main(String[] args) {
+
+        double boy, kilo, vke;
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinden) giriniz :");
+        boy= scan.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz :");
+        kilo= scan.nextDouble();
+        vke=kilo/(boy*boy);
+        System.out.println("Vücut Kitle İndeksiniz : "+ vke);
+    }
+}
